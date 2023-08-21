5ad0512d363ffc5cdb88f813004834b8c0a276c2
Igaa-gth <141727435+Igaa-gth@users.noreply.github.com>
2023-08-21 21:39:31 +0100

    Exercises 15-23

--- adventureWorksExercises.sql
+++ adventureWorksExercises.sql
@@ -94,10 +94,88 @@ union all --(union all- return all records, union- returns unique records)
 select SUM(quantity) as 'Total Quantity', locationid, shelf
 from production.productinventory
 group by rollup (locationid, shelf)
-
+order by locationid, shelf
 
 select SUM(quantity) as 'Total Quantity', locationid, shelf
 from production.productinventory
 group by grouping sets ( rollup (locationid, shelf), cube (locationid, shelf) )
+order by select SUM(quantity) as 'Total Quantity', locationid, shelf
+from production.productinventory
+group by grouping sets ( rollup (locationid, shelf), cube (locationid, shelf) )
+order by  locationid, shelf
 
+-- 15. From the following table write a query in SQL to find the total quantity for each locationid and calculate the grand-total for all locations. Return locationid and total quantity. Group the results on locationid.
+select SUM(quantity) as 'Total Quantity', locationid
+from production.productinventory
+group by rollup(locationid) 
+order by locationid
+
+---16. From the following table write a query in SQL to retrieve the number of employees for each City. Return city and number of employees. Sort the result in ascending order on city.
+select ad.city, COUNT(bu.businessentityid) as "number of employees"
+from Person.BusinessEntityAddress as bu
+INNER join person.address as ad
+ on ad.addressid= bu.addressid
+inner join person.person as pe
+ on bu.businessentityid= pe.businessentityid
+ where pe.persontype in('EM', 'SC', 'SC')
+ group by ad.city
+order by city asc
+
+---17. From the following table write a query in SQL to retrieve the total sales for each year. Return the year part of order date and total due amount. Sort the result in ascending order on year part of order date.
+select SUM(TotalDue) AS ORDERAMOUNT, DATEPART(year, OrderDate) as 'YEAR'
+from Sales.SalesOrderHeader
+ group by DATEPART(year, OrderDate)
+order by DATEPART(year, OrderDate)
+
+---18. From the following table write a query in SQL to retrieve the total sales for each year. Filter the result set for those orders where order year is on or before 2016. Return the year part of orderdate and total due amount. Sort the result in ascending order on year part of order date.
+select SUM(TotalDue) AS ORDERAMOUNT, DATEPART(year, OrderDate) as 'YEAR'
+from Sales.SalesOrderHeader
+ group by DATEPART(year, OrderDate)
+order by DATEPART(year, OrderDate)
+
+---19. From the following table write a query in SQL to find the contacts who are designated as a manager in various departments. Returns ContactTypeID, name. Sort the result set in descending order.
+select  ContactTypeID, name
+from Person.ContactType
+where name like '%manager%'
+order by ContactTypeID desc
+
+--20. From the following tables write a query in SQL to make a list of contacts who are designated as 'Purchasing Manager'. Return BusinessEntityID, LastName, and FirstName columns. Sort the result set in ascending order of LastName, and FirstName.
+select  pe.businessentityid, pe.lastname, pe.firstname
+from Person.person as pe
+join Person.BusinessEntityContact as bu
+on pe.businessentityid=bu.PersonID--- z dokumentacji  
+join person.contacttype as co
+on co.contacttypeid= bu.contacttypeid
+where co.name='Purchasing manager'
+order by lastname, firstname asc
+
+---21. From the following tables write a query in SQL to retrieve the salesperson for each PostalCode who belongs to a territory and SalesYTD is not zero. Return row numbers of each group of PostalCode, last name, salesytd, postalcode column. Sort the salesytd of each postalcode group in descending order. Shorts the postalcode in ascending order.
+select count(*) as rownumbers, lastname, salesytd, postalcode, pe.businessentityid
+from Person.person as pe
+join Sales.salesperson as sa
+on pe.businessentityid=sa.businessentityid
+join person.BusinessEntity as bu
+on bu.BusinessEntityID= pe.BusinessEntityID
+join person.BusinessEntityAddress as bea
+on bu.BusinessEntityID=bea.BusinessEntityID
+join person.Address as at
+on bea.AddressID=at.AddressID
+where TerritoryID is not null 
+group by lastname, salesytd, postalcode, pe.businessentityid
+having max(SalesYTD)>0
+
+---22. From the following table write a query in SQL to count the number of contacts for combination of each type and name. Filter the output for those who have 100 or more contacts. Return ContactTypeID and ContactTypeName and BusinessEntityContact. Sort the result set in descending order on number of contacts.
+select count(*) as count,  co.name as ContactTypeName, bu.ContactTypeID 
+from Person.Contacttype as co
+join Person.BusinessEntityContact as bu
+on co.ContactTypeID=bu.ContactTypeID
+group by bu.ContactTypeID, co.name
+HAVING COUNT(*) >= 100
+
+
+---23. From the following table write a query in SQL to retrieve the RateChangeDate, full name (first name, middle name and last name) and weekly salary (40 hours in a week) of employees. In the output the RateChangeDate should appears in date format. Sort the output in ascending order on NameInFull.
+select CAST(eh.RateChangeDate AS DATE) AS DATE, eh.rate *40 as 'weekly salary', concat (pe.firstname,' ', pe.middlename,' ', pe.lastname) as 'full name'
+from HumanResources.EmployeePayHistory as eh
+join person.person as pe
+on pe.BusinessEntityID= eh.BusinessEntityID
 
